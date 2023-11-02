fb26ef2ba0dccbd70a4e5621b49dd6673409dfcd
beumJoon <qjawns0512@daum.net>
2023-11-02 20:39:33 +0900

    55

--- src/company/command/CompanyListHandler.java
+++ src/company/command/CompanyListHandler.java
@@ -1,39 +1,26 @@
 package company.command;
 
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import company.service.CompanyPage;
+import company.service.CompanyService;
 import mvc.command.CommandHandler;
 
 public class CompanyListHandler implements CommandHandler {
-	private static final String FORM_VIEW = "/WEB-INF/view/company/CompanyListForm";
+	private CompanyService companyService = new CompanyService();
 
 	@Override
-	public String process(HttpServletRequest req, HttpServletResponse res) throws SQLException {
-		if (req.getMethod().equalsIgnoreCase("GET")) {
-			return processForm(req, res); // HTTP GET 요청 처리
-		} else if (req.getMethod().equalsIgnoreCase("POST")) {
-			return processSubmit(req, res); // HTTP POST 요청 처리
-		} else {
-			res.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-			return null;
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		String pageNoVal = req.getParameter("pageNo");
+		int pageNo = 1;
+		if (pageNoVal != null) {
+			pageNo = Integer.parseInt(pageNoVal);
 		}
+		CompanyPage companyPage = companyService.getCompanyPage(pageNo);
+		req.setAttribute("companyPage", companyPage);
+
+		return "/WEB-INF/view/company/CompanyListForm.jsp";
 	}
 
-	private String processForm(HttpServletRequest req, HttpServletResponse res) throws SQLException {
-		return FORM_VIEW;
-	}
-
-	private String processSubmit(HttpServletRequest req, HttpServletResponse res) throws SQLException {
-		Map<String, Boolean> errors = new HashMap<>();
-		req.setAttribute("errors", errors);
-		if (!errors.isEmpty()) {
-			return FORM_VIEW;
-		}
-		return "/WEB-INF/view/company/CompanySearchform.jsp";
-	}
 }
--- src/company/service/CompanyPage.java
+++ src/company/service/CompanyPage.java
@@ -12,34 +12,60 @@ public class CompanyPage {
 	private int totalPages;
 	private int startPage;
 	private int endPage;
-	
+
 	public CompanyPage(int total, int currentPage, int size, List<Company> content) {
 		this.total = total;
 		this.currentPage = currentPage;
 		this.content = content;
-		if(total==0) {
-			totalPages=0;
-			startPage=0;
-			endPage=0;
-		}else {
-			totalPages=total /size;
-			if(total%size>0) {
+		if (total == 0) {
+			totalPages = 0;
+			startPage = 0;
+			endPage = 0;
+		} else {
+			totalPages = total / size;
+			if (total % size > 0) {
 				totalPages++;
 			}
-			int modVal = currentPage %5;
-			startPage = currentPage/5*5+1;
-			if(moVal==0) startPage -=5;
-			
-			endPage = startPage +4;
-			if(endPage>totalPages) endPage = totalPages;
+			int modVal = currentPage % 5;
+			startPage = currentPage / 5 * 5 + 1;
+			if (modVal == 0)
+				startPage -= 5;
+
+			endPage = startPage + 4;
+			if (endPage > totalPages)
+				endPage = totalPages;
 		}
 	}
 
 	public int getToal() {
 		return total;
 	}
-	
-	public boolean hasNo() {
-		return total;
+
+	public boolean hasNoCompanies() {
+		return total == 0;
+	}
+
+	public boolean hasCompanies() {
+		return total > 0;
+	}
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+	public int getTotalPages() {
+		return totalPages;
+	}
+
+	public List<Company> getContent() {
+		return content;
+	}
+
+	public int getStartPage() {
+		return startPage;
+	}
+
+	public int getEndPage() {
+		return endPage;
 	}
 }
