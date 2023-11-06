c186b73251621d98b2d3e20031cd0354e350573c
XeonHan <sj0629h@gmail.com>
2023-11-06 20:50:54 +0900

    /

--- src/order/service/OrderRegisterService.java
+++ src/order/service/OrderRegisterService.java
@@ -3,6 +3,7 @@ package order.service;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 import jdbc.JdbcUtil;
 import jdbc.connection.ConnectionProvider;
@@ -58,9 +59,70 @@ public class OrderRegisterService {
 				req.getPrice(), req.getCompanyName(), req.getStorageName(), req.getOrderDate(), req.getProgress());
 	}
 
-	public List<String> getAllItemNames() {
-		// TODO Auto-generated method stub
-		return null;
+	public Map<String, Integer> getItemNamesWithUnitPrice() {
+        Connection conn = null;
+        try {
+            conn = ConnectionProvider.getConnection();
+            conn.setAutoCommit(false);
+            Map<String, Integer> itemDetails = orderDao.getItemNamesWithUnitPrice(conn);
+            conn.commit();
+            return itemDetails;
+        } catch (SQLException e) {
+            JdbcUtil.rollback(conn);
+            throw new RuntimeException(e);
+        } finally {
+            JdbcUtil.close(conn);
+        }
+    }
+
+
+	public List<String> getCompanyList() {
+		Connection conn = null;
+		try {
+			conn = ConnectionProvider.getConnection();
+			conn.setAutoCommit(false);
+			List<String> companyList = orderDao.getCompanyList(conn);
+			conn.commit();
+			return companyList;
+		} catch (SQLException e) {
+			JdbcUtil.rollback(conn);
+			throw new RuntimeException(e);
+		} finally {
+			JdbcUtil.close(conn);
+		}
 	}
+
+	public List<String> getStorageList() {
+		Connection conn = null;
+		try {
+			conn = ConnectionProvider.getConnection();
+			conn.setAutoCommit(false);
+			List<String> storageList = orderDao.getStorageList(conn);
+			conn.commit();
+			return storageList;
+		} catch (SQLException e) {
+			JdbcUtil.rollback(conn);
+			throw new RuntimeException(e);
+		} finally {
+			JdbcUtil.close(conn);
+		}
+	}
+
+	public List<String> getAllItemNames() {
+		Connection conn = null;
+		try {
+			conn = ConnectionProvider.getConnection();
+			conn.setAutoCommit(false);
+			List<String> itemNames = orderDao.getAllItemNames(conn);
+			conn.commit();
+			return itemNames;
+		} catch (SQLException e) {
+			JdbcUtil.rollback(conn);
+			throw new RuntimeException(e);
+		} finally {
+			JdbcUtil.close(conn);
+		}
+	}
+	
 
 }
